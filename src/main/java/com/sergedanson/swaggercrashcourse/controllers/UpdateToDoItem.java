94c9352d699ab9bbe9519646875fab77925d3b2e
Serge Danson <sergedanson@gmail.com>
2022-07-20 23:35:37 +0200

    add security and use codegen

--- src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
+++ src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
@@ -5,6 +5,9 @@ import com.sergedanson.swaggercrashcourse.models.ToDoItem;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +19,14 @@ public class UpdateToDoItem {
             operationId = "updateTodo",
             summary = "update todo",
             description = "this end point updates a todo",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description="Req body description",
-                    content = @Content(schema = @Schema(implementation = ToDoItem.class))
-            )
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description="Req body description",
+                    content = @Content(schema = @Schema(implementation = ToDoItem.class))),
+            responses = {@ApiResponse(
+                    responseCode =  "200",
+                    content = @Content(schema = @Schema(implementation = ToDoItem.class), mediaType = MediaType.APPLICATION_JSON_VALUE ),
+                    description = "Success desc"
+            )}
     )
     public ResponseEntity<Object> updateToDoItem(@PathVariable String toDoItemId,
                                                  @RequestBody ToDoItem toDoItem,
