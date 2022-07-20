e05b4b2341b9d11c90005c4a18bdb70c73c45eb7
Serge Danson <sergedanson@gmail.com>
2022-07-20 19:47:01 +0200

    add @Operation and @OpenAPIDefinition

--- src/main/java/com/sergedanson/swaggercrashcourse/SwaggercrashcourseApplication.java
+++ src/main/java/com/sergedanson/swaggercrashcourse/SwaggercrashcourseApplication.java
@@ -1,9 +1,20 @@
 package com.sergedanson.swaggercrashcourse;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Spring boot API with Swagger", version = "1.0", description = "Spring boot API with Open API Specification"),
+		servers = {@Server(url = "localhost:8080"), @Server(url = "https://sergedanson.com") },
+		tags = {@Tag(name="TodoItems", description = "Todo items")}
+
+)
 public class SwaggercrashcourseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
+++ src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
@@ -1,12 +1,16 @@
 package com.sergedanson.swaggercrashcourse.controllers;
 
 import com.sergedanson.swaggercrashcourse.models.ToDoItem;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UpdateToDoItem {
     @PutMapping(value = "/todos/{toDoItemId}")
+    @Operation(
+            tags = {"TodoItems"}
+    )
     public ResponseEntity<Object> updateToDoItem(@PathVariable String toDoItemId,
                                                  @RequestBody ToDoItem toDoItem,
                                                  @CookieValue(required = false) String canEdit,
