4322d208e82e792061ec1c270942bf1589c6652c
Serge Danson <sergedanson@gmail.com>
2022-07-20 23:10:52 +0200

    describing request param

--- src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
+++ src/main/java/com/sergedanson/swaggercrashcourse/controllers/UpdateToDoItem.java
@@ -1,7 +1,10 @@
 package com.sergedanson.swaggercrashcourse.controllers;
 
+import com.sergedanson.swaggercrashcourse.models.Activity;
 import com.sergedanson.swaggercrashcourse.models.ToDoItem;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class UpdateToDoItem {
     @PutMapping(value = "/todos/{toDoItemId}")
     @Operation(
-            tags = {"TodoItems"}
+            tags = {"TodoItems"},
+            operationId = "updateTodo",
+            summary = "update todo",
+            description = "this end point updates a todo",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description="Req body description",
+                    content = @Content(schema = @Schema(implementation = ToDoItem.class))
+            )
     )
     public ResponseEntity<Object> updateToDoItem(@PathVariable String toDoItemId,
                                                  @RequestBody ToDoItem toDoItem,
---
+++ src/main/java/com/sergedanson/swaggercrashcourse/models/Activity.java
@@ -0,0 +1,14 @@
+package com.sergedanson.swaggercrashcourse.models;
+
+public class Activity {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
